c16e04696605fbaff5ffad6d8b6401fc783e5192
Ivan Muniz <ivanmrosa@gmail.com>
2018-01-06 14:07:30 -0200

    adding static root

--- myLanguages/settings.py
+++ myLanguages/settings.py
@@ -144,6 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 9000
 CORS_ORIGIN_ALLOW_ALL = True
